e4f92c1d3ed47e1e9ded34906b9e3221e55a91ee
Jack Taylor <44332148+jackTabsCode@users.noreply.github.com>
2023-10-17 10:04:11 -0700

    Use roselect for createSelector

--- src/shared/selectors/player.lua
+++ src/shared/selectors/player.lua
@@ -1,12 +1,12 @@
 local ReplicatedStorage = game:GetService("ReplicatedStorage")
 
-local Reflex = require(ReplicatedStorage.Packages.Reflex)
 local SharedState = require(ReplicatedStorage.shared.types.state)
 
+local roselect = require(ReplicatedStorage.shared.vendor.roselect)
 local selectCharacter = require(script.Parent.character).selectCharacter
 
 function selectPlayer(name: string)
-	return Reflex.createSelector(selectCharacter(name), function(character: SharedState.Character)
+	return roselect.createSelector(selectCharacter(name), function(character: SharedState.Character)
 		if not character then
 			return nil
 		end
